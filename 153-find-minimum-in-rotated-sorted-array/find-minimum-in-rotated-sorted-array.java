58ab0b7e5bbd1d66f62f9dad78aec9e42f161268
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-02 23:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (86.32%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,17 @@
+//logic=> use BS as we wnt to search in sorted array, first we will see if the mid is greater than 
+//end if yes it means after mid there will be ascending array where the start element will be the
+ //lowest otherwise the first half will be the sorted array and the index 0 will be the smallest
 class Solution {
     public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+        int start= 0,end=nums.length-1;
+        while(start<end){
+            int mid =start + (end-start)/2;
+            if(nums[mid]>nums[end]){
+                start = mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        return nums[start];
     }
 }
