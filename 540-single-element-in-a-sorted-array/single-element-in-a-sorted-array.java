9a33439c53dcf4e602256c3e713c59e1fc028e0a
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-03 13:46:34 +0530

    Time: 0 ms (100.00%) | Memory: 50.2 MB (67.50%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,11 +1,30 @@
-//logic=> XOR of number with itself gives '0' and with zero gives the number back.hence the 
-//duplicates will become zero and one element will remain
+//logic=>sorted array apply BS,there will be 3 cases mid can be single element and left or right 
+//if mids post and prefix is not same it means mid is the element,array can be even or odd
+// for even and odd if mid & mids+1 and mid & mids previous is same respectiverly it means single element lies in right bcoz duplicates will occur left side
+// else search in left array
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int ans=0;
-        for(int i=0;i<nums.length;i++){
-            ans =ans ^ nums[i];
+         int n= nums.length;
+         if (n==1)
+            return nums[0];
+         if(nums[0]!=nums[1]){
+             return nums[0];
+         }   
+         if(nums[n-1]!=nums[n-2]){
+             return nums[n-1];
+         }
+        int start=0,end=n-1;
+        while(start<=end){
+            int mid= start +(end-start)/2;
+            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
+                return nums[mid];
+            }
+            if((mid%2==0 && nums[mid] == nums[mid+1]) ||(mid%2==1 && nums[mid] == nums[mid-1])){
+                start =mid+1;
+            }else{
+                end = mid-1;
+            }
         }
-        return ans;
+        return -1;
     }
 }
