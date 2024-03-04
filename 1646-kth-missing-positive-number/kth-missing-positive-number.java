3c586812fa335af289cc246b9b4501f068d35c9d
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-05 00:17:28 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (60.64%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,15 +1,15 @@
+//logic=> iterate through the array if the ith number is less than the k value ,it means the no. 
+//has taken place in the sequence of the missing no.s so increase the value of 'k' at last print the
+// value of updated k
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int low = 0, high = arr.length - 1;
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int missing = arr[mid] - (mid + 1);
-            if (missing < k) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<=k){
+                k++;
+            }else{
+                break;
             }
         }
-        return k + high + 1;
+        return k;
     }
 }
