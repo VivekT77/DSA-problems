2b30b327fe8a255c08172b37e3ea1542137e417f
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-05 20:38:19 +0530

    Time: 10 ms (15.84%) | Memory: 45.8 MB (58.77%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -2,14 +2,11 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 //if we start from (0,0) then we will not be sure whether start for row-wise or col-wise ,so start
 // from right corner like 15 from example 1     
-        int row =0, col = matrix[0].length - 1;
-        while (row < matrix.length && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true;
-            }else if(matrix[row][col] < target) {
-                 row++;
-            }else{ 
-                col--;
+        int n = matrix.length, m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == target)
+                    return true;
             }
         }
         return false;
