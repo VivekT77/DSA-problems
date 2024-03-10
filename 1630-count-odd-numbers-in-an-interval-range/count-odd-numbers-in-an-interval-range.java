819b18240ca8fdbc017f6bed914fabf1a93764a1
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-11 00:05:52 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (59.42%) - LeetSync

--- 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
+++ 1630-count-odd-numbers-in-an-interval-range/count-odd-numbers-in-an-interval-range.java
@@ -1,16 +1,10 @@
 class Solution {
     public int countOdds(int low, int high) {
-// if both no.s even then odd no will be even's like 8 and 10
+// if high-low is even then no of odd are odds        
         if(low%2==0 && high%2==0){
             return (high-low)/2;
-        }
-// if both no.s odd then odd no.s will be odd like 3 and 7    
-        else if(low%2==1 && high%2==1){
+        }else{
             return (high-low)/2 + 1 ;
-        }
-// if both no.s even then odd no will be even's like 4 and 7        
-        else{
-            return (high-low +1)/2;
         }
     }
 }
