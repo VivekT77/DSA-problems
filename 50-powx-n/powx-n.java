2b51984ac3d88d58e0c2b3c2ac2d777b51207322
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-11 14:56:56 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (55.12%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,8 +1,17 @@
+//recursion
+//logic=> if the n is even then x^(n/2) + x^(n/2)...like 2^10 = 2^5 + 2^5
+// if the n is odd then x + x^(n/2) + x^(n/2)...like 2^7 = 2 + 2^3 + 2^3
 class Solution {
     public double myPow(double x, int n) {
         if(n==0){
             return 1;
         }
-        return Math.pow(x,n);
+        if (n < 0) {
+            return 1 / (x * myPow(x, -n-1 ));
+        }
+        if(n%2==0){
+            return myPow(x*x,n/2);
+        }
+        return x * myPow(x*x,(n-1)/2);
     }
 }
