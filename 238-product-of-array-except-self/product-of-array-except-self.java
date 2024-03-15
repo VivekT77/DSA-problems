74883344b75eb2bfde41d61baa87e857857041f9
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-15 11:23:53 +0530

    Time: 3 ms (16.99%) | Memory: 55 MB (9.67%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -0,0 +1,21 @@
+//logic=> calculate the preproduct and post from the element and multiply the both to get the whole product,the initial elements of the arrays will be "1" prefix will be the product of previous elements therefore i-1 and post will be i+1
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n=nums.length;
+        int [] pre_sum = new int[n]; 
+        int [] post_sum = new int[n];
+        pre_sum[0]=1;
+        post_sum[n-1]=1;
+        for(int i=1;i<n;i++){
+            pre_sum[i] = pre_sum[i-1] * nums[i-1]; // nums[i] is not bcoz we have find except it
+        }
+        for(int i=n-2;i>=0;i--){
+            post_sum[i] = post_sum[i+1] * nums[i+1]; //same here
+        }
+        int [] result =new int[n];
+        for(int i=0;i<n;i++){
+            result[i] = pre_sum[i] * post_sum[i];
+        }
+        return result;
+    }
+}
