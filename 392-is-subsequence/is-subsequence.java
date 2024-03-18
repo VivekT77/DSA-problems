9bf50bf760153ec4184e7207147e0d7f6bc3e311
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-18 23:14:45 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (91.61%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        char[] s_arr = s.toCharArray();
+        char[] t_arr = t.toCharArray();
         int i=0,j=0;
-        while(i<s.length() && j<t.length()){
-            if(s.charAt(i)==t.charAt(j)){
+        while(i<s_arr.length && j<t_arr.length){
+            if(s_arr[i]==t_arr[j]){
                 i++;
             }
             j++;
         }
-        return i==s.length(); //check whether all the characters in s has matched,it means the index  while pointer is at last index of s will be equal of s's length
+        return i==s_arr.length; //check whether all the characters in s has matched, and if the count of i == arr.length it means all the characters are present in subsequence
     }
 }
