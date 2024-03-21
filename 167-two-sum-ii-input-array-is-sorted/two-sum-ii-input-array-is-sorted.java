f0305afb38ac3e3c04a744ca60f0a002dab64218
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-21 20:20:38 +0530

    Time: 1 ms (99.51%) | Memory: 47.4 MB (17.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -3,10 +3,10 @@ class Solution {
          int start=0,end=numbers.length-1;
          while(start<end){
             int sum = numbers[start] + numbers[end];
-            if(sum>target){
+            if(sum==target){
+                return new int[]{start+1,end+1}; //bcoz it's 1-indexed array therefore add +1 to both
+            }else if(sum>target){
                 end--;
-            }else if(sum==target){
-                return new int[]{start+1,end+1}; //bcoz it's 1-indexed array therefore add +1 to both 
             }else{
                 start++;
             }
