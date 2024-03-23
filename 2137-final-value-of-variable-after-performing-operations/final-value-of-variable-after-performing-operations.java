0e6b26e087d3f9518843b220fa9cccdea7d0c123
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-03-23 10:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (31.43%) - LeetSync

--- 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -1,8 +1,9 @@
+// without using built in functions
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
         int count=0;
         for(String str:operations){
-            if(str.equals("++X") ||str.equals("X++")){
+            if(str.charAt(1) == '+'){ //look at 2 index of a  particular string whether to add or minus
                 count++;
             }else{
                 count--;
