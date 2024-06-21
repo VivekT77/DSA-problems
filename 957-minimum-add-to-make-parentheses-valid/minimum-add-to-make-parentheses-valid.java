d9851cf20cddd8c3a17abb4930f3359df01da489
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-06-22 01:15:06 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (50.82%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -1,17 +1,31 @@
 class Solution {
     public int minAddToMakeValid(String s) {
-        Stack<Character> st = new Stack<>(); //eg: "(()("
+        // stack approach
+        // Stack<Character> st = new Stack<>(); //eg: "(()("
+        // for(char ch : s.toCharArray()){
+        //     if(ch==')'){
+        //         if(!st.isEmpty() && st.peek()=='('){ 
+        //             st.pop();     //as they will cancel each other
+        //         }else{
+        //             st.push(ch); //otherwise push it like ")))"
+        //         }
+        //     }else{
+        //         st.push(ch); 
+        //     }
+        // }
+        // return st.size(); 
+
+        int open=0;
+        int close=0;
         for(char ch : s.toCharArray()){
-            if(ch==')'){
-                if(!st.isEmpty() && st.peek()=='('){ 
-                    st.pop();     //as they will cancel each other
-                }else{
-                    st.push(ch); //otherwise push it like ")))"
-                }
+            if(ch=='('){
+                open++;
+            }else if(ch==')' && open==0){
+                close++;
             }else{
-                st.push(ch); 
+                open--;
             }
         }
-        return st.size();
+        return open +close;
     }
 }
