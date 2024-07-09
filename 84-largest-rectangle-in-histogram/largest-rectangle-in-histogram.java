b69aebcddabb0b53f741c2488176848342988d09
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-07-10 00:03:15 +0530

    Time: 69 ms (58.21%) | Memory: 58 MB (19.91%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,43 +1,63 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
+        Stack <Integer> st = new Stack<>();
         int max_Area = 0;
-        int n=heights.length;
-        int [] nsl = new int[n]; //to store the indices of the next smaller left element
-        int [] nsr = new int[n];  //to store the indices of the next smaller right element
+        int n = heights.length;
+        for(int i=0;i<=n;i++){
+            while(!st.isEmpty() && (i==n || heights[st.peek()]>=heights[i])){
+                int height=heights[st.peek()];
+                st.pop();
+                int width;
 
-        //for nsl
-        Stack<Integer> st = new Stack<>();
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                nsl[i]=-1;  //for left before starting
-            }else{
-                nsl[i]=st.peek();
+                if(st.isEmpty()){
+                    width=i;
+                }else{
+                    width=i-st.peek()-1;
+                } 
+            max_Area = Math.max(max_Area,height*width);
             }
             st.push(i);
-        }
-        //for nsr
-        st= new Stack<>();
-        for(int i=n-1;i>=0;i--){    //we need to traverse from back 
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                nsr[i]=n;  //"n" will be the nsr if element next to boundary is greater than the currrent
-            }else{
-                nsr[i]=st.peek();
-            }
-            st.push(i);
-        }
-        //find area
-        for(int i=0;i<n;i++){
-        int height=heights[i];
-        int width= nsr[i]-nsl[i]-1;  //j-i-1 for 3rd bar the width will be 4-1-1=2(j,i is exclude)
-        int area = height*width;
-        max_Area = Math.max(max_Area,area); //for each index it is calculated
         }
         return max_Area;
+
+        // int max_Area = 0;
+        // int n=heights.length;
+        // int [] nsl = new int[n]; //to store the indices of the next smaller left element
+        // int [] nsr = new int[n];  //to store the indices of the next smaller right element
+
+        // //for nsl
+        // Stack<Integer> st = new Stack<>();
+        // for(int i=0;i<n;i++){
+        //     while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+        //         st.pop();
+        //     }
+        //     if(st.isEmpty()){
+        //         nsl[i]=-1;  //for left before starting
+        //     }else{
+        //         nsl[i]=st.peek();
+        //     }
+        //     st.push(i);
+        // }
+        // //for nsr
+        // st= new Stack<>();
+        // for(int i=n-1;i>=0;i--){    //we need to traverse from back 
+        //     while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
+        //         st.pop();
+        //     }
+        //     if(st.isEmpty()){
+        //         nsr[i]=n;  //"n" will be the nsr if element next to boundary is greater than the currrent
+        //     }else{
+        //         nsr[i]=st.peek();
+        //     }
+        //     st.push(i);
+        // }
+        // //find area
+        // for(int i=0;i<n;i++){
+        // int height=heights[i];
+        // int width= nsr[i]-nsl[i]-1;  //j-i-1 for 3rd bar the width will be 4-1-1=2(j,i is exclude)
+        // int area = height*width;
+        // max_Area = Math.max(max_Area,area); //for each index it is calculated
+        // }
+        // return max_Area;
     }
 }
