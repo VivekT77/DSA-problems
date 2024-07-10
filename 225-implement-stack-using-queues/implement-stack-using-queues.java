3cf9bdcfda3c0a01b2739619ad799d9bc95cdcfb
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-07-10 23:59:13 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (34.32%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -2,28 +2,69 @@ import java.util.*;
 
 class MyStack {
     
-    Queue<Integer> q= new LinkedList<>();;
-    public MyStack() {
+    Queue<Integer> q1= new LinkedList<>();
+    Queue<Integer> q2= new LinkedList<>();
 
+    public MyStack() {
+        
     }
     
     public void push(int x) {
-        q.offer(x);
-        for (int i = 0; i < q.size() - 1; i++) {
-            q.offer(q.poll());
+        if(!q1.isEmpty()){     //which ever queue is empty simply add in it
+            q1.add(x);
+        }else{
+            q2.add(x);
         }
     }
     
     public int pop() {
-        return q.poll();
+        if(empty()){
+            return -1;
+        }
+        int top=-1;
+
+        if(!q1.isEmpty()){    //till q1 gets empty remove the elements and add in q2 and return the top
+            while(!q1.isEmpty()){       //eg:1,2,3 and here top=3 
+                top = q1.remove();
+                if(q1.isEmpty()){   //if gets empty break the loop
+                    break;
+                }
+                q2.add(top);
+            }
+        }else{          //till q2 gets empty remove the elements and add in q1 and return the top
+            while(!q2.isEmpty()){      //eg:1,2 and here top=2
+                top=q2.remove();        //so queue will return 3,2,1 like stack 
+                if(q2.isEmpty()){
+                    break;
+                }
+                q1.add(top);
+            }
+        }
+        return top;
     }
     
-    public int top() {
-        return q.peek();
+    public int top() {       //same operation like pop function
+        if(empty()){
+            return -1;
+        }
+        int top=-1;
+
+        if(!q1.isEmpty()){
+            while(!q1.isEmpty()){
+                top = q1.remove();
+                q2.add(top);
+            }
+        }else{
+            while(!q2.isEmpty()){
+                top=q2.remove();
+                q1.add(top);
+            }
+        }
+        return top;
     }
     
     public boolean empty() {
-        return q.isEmpty();
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
 
