1825004ef795676a6a51322ef11819ad70ff7838
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-07-13 01:13:31 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (30.47%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -9,27 +9,16 @@
  * }
  */
 class Solution{
-     public  ListNode reverse(ListNode head){  //use the same approach like Q(445) just used queue DS
-        ListNode temp=head;       //and  reverse the resultant obtained LL bcoz in this carry gets 
-        ListNode prev=null; //added in next node while in Q(455) carry used to get added in prev node
-        ListNode Next;
-        while(temp!=null){
-            Next=temp.next;
-            temp.next=prev;
-            prev=temp;
-            temp=Next;
-
-        }
-        return prev;
-    }
+     //without reversing  the obtained LL
+     
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
         int carry=0;
-        ListNode temp=null;
+        ListNode dummyNode = new ListNode(0);
+        ListNode temp = dummyNode;
         while(l1 != null || l2 != null || carry != 0){
             int val1=0;;
             int val2=0;
-            if(l1!=null){  //till queue get empty pop the node from each queue & add it with carry
+            if(l1!=null){ 
                 val1 = l1.val; 
                 l1 = l1.next;
             }  
@@ -40,10 +29,10 @@ class Solution{
          int total = val1 + val2 + carry;   
          ListNode newnode = new ListNode(total%10); //make new node with the value got from the total
          carry = total/10;      //update the carry
-         newnode.next = temp;  //assign the pointers to iterate further initially temp was null
+         temp.next=newnode;  //assign the pointers to iterate further initially temp was null
          temp=newnode;
         }
 
-        return reverse(temp); 
+        return dummyNode.next; 
     }
 }
