ca5b79d5ecdda4b45dfe9c76b875d1e458561259
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-07-15 11:55:57 +0530

    Time: 61 ms (9.39%) | Memory: 54.3 MB (5.01%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.java
+++ 633-sum-of-square-numbers/sum-of-square-numbers.java
@@ -1,13 +1,26 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        long a = 0,b = (long)Math.sqrt(c);
-        while(a<=b){
-            if(a*a + b*b==c){
+        // long a = 0,b = (long)Math.sqrt(c);
+        // while(a<=b){
+        //     if(a*a + b*b==c){
+        //         return true;
+        //     }else if(a*a + b*b>c){
+        //         b--;
+        //     }else{
+        //         a++;
+        //     }
+        // }
+        // return false;
+        Set<Integer> s = new HashSet<>();
+        int high = (int)Math.sqrt(c);  
+
+        for(int i=0;i<=high;i++){   //till i reaches high,add all the squares lower than it
+            s.add(i*i);
+        }
+        for(int i=0;i<=high;i++){  //now if the remaining part of the sum is present in set  
+            int low = c-(i*i);          //then true or false
+            if(s.contains(low)){
                 return true;
-            }else if(a*a + b*b>c){
-                b--;
-            }else{
-                a++;
             }
         }
         return false;
