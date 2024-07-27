7d7e3ca045ab41f3392b14033ff9de1ca9082d16
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-07-28 01:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (6.71%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -15,42 +15,46 @@
  */
 class Solution {
     public boolean isCousins(TreeNode root, int x, int y) {
-        if(root==null){
-            return false;
-        }
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-
-        while(!q.isEmpty()){
-            int size=q.size();
-            Boolean x_exist =false;
-            Boolean y_exist =false;
-
-            for(int i=0;i<size;i++){
-                TreeNode current = q.remove();
-                if(current.val==x){
-                    x_exist=true;
-                }
-                if(current.val==y){
-                    y_exist=true;
-                }
-
-                if(current.left!=null && current.right!=null){
-                    if(current.left.val==x && current.right.val==y || current.left.val==y && current.right.val==x){
-                        return false;
-                    }
-                }
-                if(current.left!=null){
-                    q.add(current.left);
-                }
-                if(current.right!=null){
-                    q.add(current.right);
-                }
-            }
-            if(x_exist ==true && y_exist==true){
-                return true;
-            }
-        }
-        return false;
+        //2nd  approach --- the level should bw the same but of different parents 
+        TreeNode x_node = findnode(root,x);
+        TreeNode y_node = findnode(root,y);
+        return ( (level(root,x_node,0) == level(root,y_node,0)) && (!isSiblings(root,x_node,y_node)) );
     }
+
+    public TreeNode findnode(TreeNode node,int v){
+        if(node==null){
+            return null;
+        }
+        if(node.val==v){    //if found then return else search for left and right
+            return node;
+        }
+        TreeNode n = findnode(node.left,v);
+        if(n!=null){  //if it is found in left,return it otherwise go for the right part
+            return n;
+        }else{
+            return findnode(node.right,v);
+        }
+    }
+
+    public int level(TreeNode node,TreeNode x,int lev){
+        if(node==null){
+            return 0;
+        }
+        if(node==x){      //if the node and the x's or y's node is same then return the level  
+            return lev;
+        }
+        int l = level(node.left,x,lev+1);
+        if(l != 0){     
+            return l;
+        }else{
+            return level(node.right,x,lev+1);
+        }
+    }
+
+        Boolean isSiblings(TreeNode node,TreeNode x,TreeNode y){
+            if(node==null){
+                return false;
+            }
+            return (node.left==x && node.right==y || node.left==y && node.right==x || isSiblings(node.left,x,y) || isSiblings(node.right,x,y) );  
+        }   //true && true means found the nodes else one also get false then totall  false
 }
