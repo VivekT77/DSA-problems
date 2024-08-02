b45481f304baa10a6191825db9b57ba0269c895d
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-03 00:36:06 +0530

    Time: 40 ms (68.29%) | Memory: 57.2 MB (74.36%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -4,8 +4,8 @@ class Solution {
         for(int i=0;i<k;i++){
             pq.add(nums[i]);
         }
-
-        for(int i=k;i<nums.length;i++){
+        int n = nums.length;
+        for(int i=k;i<n;i++){
             if(nums[i] > pq.peek()){
                 pq.remove();
                 pq.add(nums[i]);
