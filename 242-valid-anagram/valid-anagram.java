5f77f2fe8816043755d59cbf82de42cc223ea5b3
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-04 23:28:44 +0530

    Time: 17 ms (19.34%) | Memory: 44.9 MB (10.84%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,15 +1,28 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        char[] s_ch = s.toCharArray();
-        char[] t_ch = t.toCharArray();
-        Arrays.sort(s_ch);  
-        Arrays.sort(t_ch);
-        for(int i=0;i<s_ch.length;i++){
-            if(s_ch[i]!=t_ch[i]){
-                return false;
-            }
+
+        if(s.length()!= t.length()){
+            return false;
         }
-        return true;  
+
+        HashMap<Character, Integer> m = new HashMap<>();    //ch and frequency of the particular ch is stored
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            m.put(ch , m.getOrDefault(ch,0) + 1);
+        }
+        
+        for(int i=0;i<t.length();i++){
+           char ch = t.charAt(i);
+           if(m.get(ch)!=null){         //if the freq in the map is not null  then start the operation of 
+                if(m.get(ch)==1){       // reducing the freq of the character
+                    m.remove(ch);       // if one remove the ch
+                }else{
+                    m.put(ch , m.get(ch) - 1);    //if more than one then reduce the freq of particular ch 
+                }
+           }else{
+            return false;
+           } 
+        }
+        return m.isEmpty();
     }
 }
