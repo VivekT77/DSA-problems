4c1f668798048980651ee469f7d24066e77d81aa
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-05 14:56:44 +0530

    Time: 1 ms (75.92%) | Memory: 45.3 MB (8.47%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -15,30 +15,53 @@
  */
 class Solution {
     public int findBottomLeftValue(TreeNode root) {
-        if(root==null){
-            return 0;
-        }
+    
+        //2nd approach
+        ArrayList<Integer> ans = new ArrayList<>();
+        find_value(ans,root,0);
+        return ans.get(ans.size()-1);       //return the last element as it will be the iterated at bottom
 
-        Queue<TreeNode> q= new LinkedList<>();
-        q.add(root);
-
-        int left_bottom_value=0;
-        while(!q.isEmpty()){
-
-            int levelsize = q.size();
-            left_bottom_value = q.peek().val;   //at last the left node of the subtree will be stored in the
-                                                //queue,hence return the peek 
-            for(int i=0;i<levelsize;i++){
-                TreeNode node = q.remove();
-
-                if(node.left!=null){
-                    q.add(node.left);
-                }
-                if(node.right!=null){
-                    q.add(node.right);
-                }
-            }
-        }
-        return left_bottom_value;
     }
+
+    public void find_value(ArrayList<Integer> ans , TreeNode root ,int level){
+        if(root==null){
+            return;
+        }
+
+        if(level == ans.size()){     //due to this only left nodes will get as for right the list size will
+            ans.add(root.val);         //be not equal to level value
+        }
+
+        find_value(ans,root.left,level + 1);
+        find_value(ans,root.right,level + 1);
+
+    }
+
+
+//1st approach
+        // if(root==null){
+        //     return 0;
+        // }
+
+        // Queue<TreeNode> q= new LinkedList<>();
+        // q.add(root);
+
+        // int left_bottom_value=0;
+        // while(!q.isEmpty()){
+
+        //     int levelsize = q.size();
+        //     left_bottom_value = q.peek().val;   //at last the left node of the subtree will be stored in the
+        //                                         //queue,hence return the peek 
+        //     for(int i=0;i<levelsize;i++){
+        //         TreeNode node = q.remove();
+
+        //         if(node.left!=null){
+        //             q.add(node.left);
+        //         }
+        //         if(node.right!=null){
+        //             q.add(node.right);
+        //         }
+        //     }
+        // }
+        // return left_bottom_value;
 }
