45e192cc0c472f36391003090ffc888533c4d26a
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-07 15:56:50 +0530

    Time: 1 ms (100.00%) | Memory: 45.1 MB (12.72%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.java
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<Integer> findWordsContaining(String[] words, char x) {
-        List<Integer> ans = new ArrayList<>(); 
-        for(int i=0;i<words.length;i++){ //to iterate over the array "words"
-            for(int j=0;j<words[i].length();j++){ //to iterate over the particular word in the array
-                if(words[i].charAt(j)==x){ 
-                    ans.add(i);
-                    break; //otherwise it will return all the indexes of similar letters instead of one 
-                }
-            }
-        }
-        return ans;
-
-        // List<Integer> ans = new ArrayList<>();
-        // for(int i=0;i<words.length;i++){
-        //     if(words[i].indexOf(x)!=-1){ //if it is found then add the index at which the element lies
-        //         ans.add(i);
+        // List<Integer> ans = new ArrayList<>(); 
+        // for(int i=0;i<words.length;i++){ //to iterate over the array "words"
+        //     for(int j=0;j<words[i].length();j++){ //to iterate over the particular word in the array
+        //         if(words[i].charAt(j)==x){ 
+        //             ans.add(i);
+        //             break; //otherwise it will return all the indexes of similar letters instead of one 
+        //         }
         //     }
         // }
         // return ans;
+
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0;i<words.length;i++){
+            if(words[i].indexOf(x)!=-1){ //if it is found then add the index at which the element lies
+                ans.add(i);
+            }
+        }
+        return ans;
     }
 }
