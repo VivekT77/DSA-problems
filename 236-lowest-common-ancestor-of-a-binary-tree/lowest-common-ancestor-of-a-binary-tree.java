6afb6ca5472beaebabd9148a4b42a1bd35b48a4c
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-08 23:02:34 +0530

    Time: 7 ms (56.62%) | Memory: 44.7 MB (56.47%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root==null){
+            return null;
+        }
+        if(root == p || root == q){     //example no.3
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);  
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        if(left !=null && right!=null){     //we know that if the p and q are found at a particular 
+            return root;               //node then no need to go to their subtrees bcoz it will be LCA
+        }                           //and the node can be LCA of itself
+        
+        return right==null ? left : right;      //if p is found in left but q is not found in right
+    }                                       //then it will return null to its root and q will be at 
+}                                      //left side only in subtree of p
