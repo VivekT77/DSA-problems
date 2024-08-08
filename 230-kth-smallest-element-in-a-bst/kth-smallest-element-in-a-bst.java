8cf3dd4034714ab273504800db72ec5c6de55a83
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-08 23:40:35 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (21.25%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -15,19 +15,26 @@
  */
 class Solution {
 
-    ArrayList<Integer> ans = new ArrayList<>();
-    public void inorder(TreeNode root){
+    int count=0;
+    public int kthSmallest(TreeNode root, int k) {
+        //2nd approach
+        return Helper(root,k).val;
+    }
+
+    public TreeNode Helper(TreeNode root, int k){
         if(root==null){
-            return;
+            return null;
         }
 
-        inorder(root.left);
-        ans.add(root.val);
-        inorder(root.right);
-    }
-    public int kthSmallest(TreeNode root, int k) {
-       
-        inorder(root);
-        return ans.get(k-1);        //as arraylist is 0 -indexed
-    }
+        TreeNode left = Helper(root.left,k);    //as we are performing inorder therefore iterate left
+        if(left!=null){         //means somewhere in left subtree node is been found return left
+            return left;
+        }
+        
+        count++;        //it keeps record of the smallest no. frequency
+        if(count==k){       //count=1=>k=1 , count=2,k=2 => count=3,k=3
+            return root;
+        }
+        return Helper(root.right,k);    //if not found in left then find in right
+    } 
 }
