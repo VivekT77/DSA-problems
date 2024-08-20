8677c0863bb485d19a5521f747b95cad71f7d4ff
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-20 13:46:14 +0530

    Time: 22 ms (67.88%) | Memory: 60.8 MB (31.06%) - LeetSync

--- 2245-destroying-asteroids/destroying-asteroids.java
+++ 2245-destroying-asteroids/destroying-asteroids.java
@@ -1,15 +1,33 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
+    // 1st approach = O(nlogn)
         Arrays.sort(asteroids);
         long m = mass;
+
         for(int i=0;i<asteroids.length;i++){
             if(asteroids[i]<=m){
                 m += asteroids[i];
             }else{
                 return false;
             }
+            if(mass > 100000){
+                return true;
+            }
 
         }
         return true;
+
+    // //  2nd approach = O(N)   
+    //     Stack<Integer> s = new Stack<>();  //in 1st approch we sort  so to reduce TC used stack
+    //     long m = mass;
+
+    //     for( int asteroid : asteroids){
+    //         s.push(asteroid);
+
+    //         while(!s.isEmpty() && s.peek() <= m){   //compare the top element with the given mass and 
+    //             m += s.pop();     //update the mass and at last if stack is empty then true otherwise false
+    //         }
+    //     }
+    //     return s.isEmpty();
     }
 }
