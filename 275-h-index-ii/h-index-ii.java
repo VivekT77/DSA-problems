115fda3fbda6f3e1cd7024d67625adcbca7f9d3a
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-21 13:16:48 +0530

    Time: 3 ms (7.14%) | Memory: 49 MB (5.55%) - LeetSync

--- 275-h-index-ii/h-index-ii.java
+++ 275-h-index-ii/h-index-ii.java
@@ -1,13 +1,24 @@
 class Solution {
     public int hIndex(int[] citations) {
+        // 2nd approach
         int n = citations.length;
+        int [] freq = new int[n+1];
         
-        List<Integer> l = new ArrayList<>();
-        for(int i=0;i<citations.length;i++){
-            if(citations[i]>= n-i){
-                l.add(citations[i]);
+        for(int num : citations){
+            if(num >= n){
+                freq[n]++;
+            }else{
+                freq[num]++;
             }
         }
-        return l.size();
+
+        int count =0;
+        for(int i=n;i>=0;i--){
+            count += freq[i];
+            if(count >= i){
+                return i;
+            } 
+        }
+        return 0;
     }
 }
