c5694161b1ffb1e3c7162c2b113af922c5a74b2f
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-08-28 13:48:19 +0530

    Time: 76 ms (15.98%) | Memory: 59.7 MB (35.09%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,25 +1,17 @@
 class Solution {
 
-    public class Pair implements Comparable<Pair>{      //similar to the Priority queue questions
-        int index;                              //initialize , make constructor,override the compareTo method
+    public class Pair{      //similar to the Priority queue questions
+        int index;                              //initialize , make constructor
         int value;
 
         Pair(int index,int value){
             this.index=index;
             this.value=value;
         }
-
-        @Override
-        public int compareTo(Pair p2){
-            //for ascending
-            // return this.value - p2.value;
-            //for decending - we want max element from the window
-            return p2.value - this.value;
-        }
     }
     public int[] maxSlidingWindow(int[] nums, int k) {
-        
-        PriorityQueue<Pair> pq= new PriorityQueue<>();
+
+        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b) -> b.value-a.value);
         int n = nums.length;
         int[] ans = new int[n-k+1];     //size should be of no.of windows
 
