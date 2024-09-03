a5313db751108c8d03d088d7a1112923af4ffde7
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-09-04 00:26:30 +0530

    Time: 5 ms (42.94%) | Memory: 45.9 MB (7.46%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -1,16 +1,17 @@
 class Solution {
     public int singleNumber(int[] nums) {
 
+//2nd approach to iterate over the map
         HashMap<Integer,Integer> m = new HashMap<>();
         for(int n : nums){
             m.put(n,m.getOrDefault(n,0) + 1);
         }
 
-        for(Map.Entry<Integer,Integer> ans : m.entrySet()){
-            if(ans.getValue() == 1){
-                return ans.getKey();
+        for(int i : m.keySet()){        //to make the keys of the key,value pair in one set
+            if(m.get(i)==1){            //to gets the value for that particular key
+                return i;
             }
         }
-        return -1;
+        return 0;
     }
 }
