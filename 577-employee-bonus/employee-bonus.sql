b6b005966e412f1a22e0981825e33730d7ab5acc
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-09-07 00:40:07 +0530

    Time: 737 ms (99.08%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,7 @@
 # Write your MySQL query statement below
+
+-- 2nd approach where "using" is use instead of "ON"
+
 SELECT E.name , B.bonus FROM Employee E
-LEFT JOIN Bonus B ON E.empId = B.empId
+LEFT JOIN Bonus B USING (empId) 
 WHERE B.bonus<1000 OR B.bonus IS NULL; 
