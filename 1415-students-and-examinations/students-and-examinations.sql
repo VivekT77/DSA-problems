6e27b91ab087b051347aefb04fe96ac96fed2017
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2024-09-10 00:58:41 +0530

    Time: 965 ms (33.61%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -4,5 +4,5 @@ FROM Students AS st
 JOIN Subjects AS su
 LEFT JOIN Examinations AS e
 ON st.student_id = e.student_id AND su.subject_name = e.subject_name
-GROUP BY student_id , subject_name
+GROUP BY st.student_id , st.student_name , su.subject_name
 ORDER BY student_id ASC;
