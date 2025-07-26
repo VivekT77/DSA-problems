309a6469bf5a6694d779d8b08d95e60672947b8f
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-07-27 02:35:29 +0530

    Time: 5 ms (16.26%) | Memory: 45.3 MB (92.86%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.java
@@ -1,21 +1,24 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int n= nums.length;
+        int n = nums.length;
         int[] result = new int[n-k+1];
 
         for(int i=0;i<=n-k;i++){
-            int max = nums[i];
-            boolean isConsecutive_Sorted = true; 
+            int[] subarray = new int[k];
+            for(int j=0;j<k;j++){
+                subarray[j] = nums[i+j];
+            }
+
+            boolean isConsecutive_Sorted = true;
             for(int j=1;j<k;j++){
-                if(nums[i+j] != nums[i+j-1] +1){
+                if(subarray[j] != subarray[j-1] + 1){
                     isConsecutive_Sorted = false;
                     break;
                 }
-                max= Math.max(max,nums[i+j]);
             }
 
             if(isConsecutive_Sorted){
-                result[i] = max;
+                result[i] = subarray[k-1]; 
             }else{
                 result[i] = -1;
             }
@@ -23,3 +26,4 @@ class Solution {
         return result;
     }
 }
+
