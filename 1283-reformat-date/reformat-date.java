9d7a40b667a82c074862b4fcabdbfd49f618df20
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-01 01:34:21 +0530

    Time: 2 ms (82.10%) | Memory: 42 MB (57.61%) - LeetSync

--- 1283-reformat-date/reformat-date.java
+++ 1283-reformat-date/reformat-date.java
@@ -1,16 +1,6 @@
 class Solution {
     public String reformatDate(String date) {
 
-        String[] parts = date.split(" ");
-        String day = parts[0];
-        String month = parts[1];
-        String year = parts[2];
-
-        day = day.substring(0,day.length()-2);
-        if(day.length()<2){
-            day = "0" + day;
-        }
-
         HashMap<String,String> m = new HashMap<>();
         m.put("Jan","01");
         m.put("Feb","02");
@@ -25,8 +15,67 @@ class Solution {
         m.put("Nov","11");
         m.put("Dec","12");
 
-        month = m.get(month);
+        String[] parts = date.split(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append(parts[2] + "-");
+        sb.append(m.get(parts[1]) + "-");
 
-        return year + "-" + month + "-" + day;
+        String day = "";
+        if(parts[0].contains("st")){
+            day = parts[0].replace("st","");
+        }
+        else if(parts[0].contains("nd")){
+            day = parts[0].replace("nd","");
+        }
+        else if(parts[0].contains("rd")){
+            day = parts[0].replace("rd","");
+        }
+        else{
+            day = parts[0].replace("th","");
+        }
+
+
+        if(day.length() == 1){
+            day = "0" + day;
+        }
+
+        sb.append(day);
+        return sb.toString();
+
+
+
+
+
+
+
+
+
+        // String[] parts = date.split(" ");
+        // String day = parts[0];
+        // String month = parts[1];
+        // String year = parts[2];
+
+        // day = day.substring(0,day.length()-2);
+        // if(day.length()<2){
+        //     day = "0" + day;
+        // }
+
+        // HashMap<String,String> m = new HashMap<>();
+        // m.put("Jan","01");
+        // m.put("Feb","02");
+        // m.put("Mar","03");
+        // m.put("Apr","04");
+        // m.put("May","05");
+        // m.put("Jun","06");
+        // m.put("Jul","07");
+        // m.put("Aug","08");
+        // m.put("Sep","09");
+        // m.put("Oct","10");
+        // m.put("Nov","11");
+        // m.put("Dec","12");
+
+        // month = m.get(month);
+
+        // return year + "-" + month + "-" + day;
     }
 }
