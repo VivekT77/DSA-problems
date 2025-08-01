88d77a53d80650049d0e6df38b6a08f3ee70b2d5
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-01 19:14:54 +0530

    Time: 13 ms (76.36%) | Memory: 49.1 MB (25.73%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,17 +6,12 @@ class Solution {
             m.put(num,m.getOrDefault(num,0) + 1);
         }
 
-        PriorityQueue<Integer> min_pq = new PriorityQueue<>((a,b) -> m.get(a)-m.get(b));
-        for(int num:m.keySet()){
-            min_pq.add(num);
-            if(min_pq.size()>k){
-                min_pq.poll();
-            }
-        }
+        PriorityQueue<Integer> max_pq = new PriorityQueue<>((a,b) -> m.get(b)-m.get(a));
+        max_pq.addAll(m.keySet());
 
         int[] topK = new int[k];
-        for(int i=k-1;i>=0;i--){
-            topK[i] = min_pq.poll();
+        for(int i=0;i<k;i++){
+            topK[i] = max_pq.poll();
         }
         return topK;
     }
