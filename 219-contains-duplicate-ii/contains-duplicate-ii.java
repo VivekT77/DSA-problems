ee582aaad62cf4064181f1e8f1c21bceac9cd730
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-04 20:34:10 +0530

    Time: 21 ms (78.21%) | Memory: 58.6 MB (92.07%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -4,7 +4,7 @@ class Solution {
         int n = nums.length;
         HashMap<Integer,Integer> freq = new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
+        for(int i=0;i<n;i++){
             if(freq.containsKey(nums[i]) && (i-freq.get(nums[i]) <= k)){
                 return true;
             }
