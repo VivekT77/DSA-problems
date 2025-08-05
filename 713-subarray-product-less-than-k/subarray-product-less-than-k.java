3296459c684bd5d7ab02cbf95045d74cfe95d420
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-05 23:57:35 +0530

    Time: 857 ms (9.41%) | Memory: 47.9 MB (49.68%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.java
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.java
@@ -1,21 +1,19 @@
 class Solution {
     public int numSubarrayProductLessThanK(int[] nums, int k) {
-        if (k <= 1) return 0; //there can't be postive subarrays with such k value
+        int n = nums.length;
         int count=0;
-        int product = 1;
-        int start = 0;
-        int end = 0;
-
-        while(end<nums.length){ // till it reaches end,calculate the product by multiplying the last element of the particular subarray       
-            product *= nums[end]; 
-
-         while(product >= k){
-            product /= nums[start]; //to get the last digit cutoff we divide it,same way divide the k value with the starting element of the particular subarray
-            start++; //move forward to add the next index in the sliding window
-         }
-         count += (end - start)+ 1; // to count the no. of subarrays , +1 is done so that it can count the last itself as a single subarray like 5,10,2 in the increasing process
-         end++;
-      }
-      return count;
+        for(int i=0;i<n;i++){
+            int product=1;
+            for(int j=i;j<n;j++){
+                product *= nums[j]; 
+            if(product<k){
+                count++;
+            }
+            else{
+                break;
+            }
+        }
+        }
+        return count;
     }
 }
