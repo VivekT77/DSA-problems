6ae7a54b63e3704286f326e89be381ff8aae770a
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-13 13:25:54 +0530

    Time: 28 ms (92.79%) | Memory: 64.7 MB (8.90%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,32 +1,26 @@
 class Solution {
-
-    public class Pair{      //similar to the Priority queue questions
-        int index;                              //initialize , make constructor
-        int value;
-
-        Pair(int index,int value){
-            this.index=index;
-            this.value=value;
-        }
-    }
     public int[] maxSlidingWindow(int[] nums, int k) {
-
-        PriorityQueue<Pair> pq= new PriorityQueue<>((a,b) -> b.value-a.value);
         int n = nums.length;
-        int[] ans = new int[n-k+1];     //size should be of no.of windows
+        int[] ans = new int[n-k+1];
 
-        for(int i=0;i<k;i++){
-            pq.add(new Pair(i,nums[i]));        //initially add the k points in PQ
-        }
-        ans[0] = pq.peek().value;           //the start element in the PQ will be the max hence put in ans 
+        //we have use dq to store the indices of the values
+        Deque<Integer> dq = new ArrayDeque<>();   //deque is interface , can be implemented by
+        for(int i=0;i<n;i++){                  // by ArratDeque class or linkedlist
 
-        for(int i=k;i<n;i++){
-            while(pq.size() > 0 && pq.peek().index <=(i-k)){  //check whether the peek element gets out   
-                pq.remove();           //from the size of itertaing windows at each point,if there remove it
+            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i]){
+                dq.pollLast();      //compare the current value with the other values in dq,
+            }                       //remove the values which are less than it
+            
+            dq.offer(i);        //after that add the value in deque
+
+            if(dq.peek() <= i-k){  //now see if first element is out of the window size or not
+                dq.poll();              //if there remove it
             }
-            pq.add(new Pair(i,nums[i]));        //add the next element from the array to the PQ
-            ans[i-k+1] = pq.peek().value;       //start assigning from the index 1 as index 0 is filled
-        }                                       //initally only
+
+            if(i>=k-1){         //till now the max element get positioned at peek,hence add it
+                ans[i-k+1] = nums[dq.peekFirst()];
+            }
+        }
         return ans;
     }
 }
