1ccbd83653ff67608759b3e366fbffec37a3eb17
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-08-23 01:41:14 +0530

    Time: 8 ms (96.06%) | Memory: 44.7 MB (91.67%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -2,32 +2,31 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         int m = s.length();
         int n = p.length();
-        ArrayList<Integer> ans = new ArrayList<>();
 
-        int[] s_freq = new int[26]; //to store the frequency of the elements
-        int[] p_freq = new int[26];
+        ArrayList<Integer> result = new ArrayList<>();
+
+        int[] freq_s = new int[26];
+        int[] freq_p = new int[26];
 
         if(n>m){
-            return ans;
+            return result;
         }
 
-        for(char c:p.toCharArray()){    
-            p_freq[c-'a']++;
+        for(int i=0;i<n;i++){
+            freq_s[s.charAt(i) -'a']++;
+            freq_p[p.charAt(i) -'a']++;
         }
 
-        for(int i=0;i<n;i++){   //exactly store the p no. of elements in s_freq to compare
-            s_freq[s.charAt(i)-'a']++;
-        }
-
-        for(int i=0;i<=m-n;i++){    //iterate the s string till m-n as p won't reach at last
-            if(Arrays.equals(s_freq,p_freq)){       //compare and add wherever anagram is there
-                ans.add(i);     
+        for(int i=0;i<=m-n;i++){
+            if(Arrays.equals(freq_s,freq_p)){
+                result.add(i);
             }
-            if(i<m-n){        //slide the window,add the next element and remove previous
-                s_freq[s.charAt(i+n)-'a']++;
-                s_freq[s.charAt(i)-'a']--;
+
+            if(i<m-n){
+                freq_s[s.charAt(i+n) - 'a']++;
+                freq_s[s.charAt(i) - 'a']--;
             }
         }
-        return ans;
+        return result;
     }
 }
