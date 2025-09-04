3ec2f04a9e8748a6ddc8a91e56d5bc906412ba02
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-09-04 16:22:32 +0530

    Time: 31 ms (56.43%) | Memory: 51.4 MB (83.76%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,42 +1,40 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        // List<List<Integer>> L = new ArrayList<>(); 
-        // Set<List<Integer>> s = new HashSet<>();
-        // int n= nums.length;
-        //     for(int i =0;i<n-2;i++){
-        //         for(int j =i+1;j<n-1;j++){
-        //             for(int k=j+1;k<n; k++){
-        //                 if(nums[i]+nums[j]+nums[k]==0){
-        //                    s.add(Arrays.asList(nums[i], nums[j], nums[k]));
-        //            }
-        //         }
-        //     }
-        // }
-        // L.addAll(s);
-        // return L;
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
 
-        List<List<Integer>> L = new ArrayList<>(); 
-        Set<List<Integer>> s = new HashSet<>();  //to store the lists as triplets & add in ans 
-        int n= nums.length;
-        Arrays.sort(nums); //to make the searching fast ,do sort
-        for(int i=0;i<n;i++){
-            // now use the two sum or two pointer approach
-            int j = i+1;
-            int k = n-1;
-            while(j<k){
-                int sum =nums[i]+nums[j]+nums[k];
-                if(sum==0){
-                    s.add(Arrays.asList(nums[i],nums[j],nums[k]));   //we need to store the
-                    j++;        //possibilities of the nums in list format ,so Arrays.asList()
-                    k--;        //makes a new list into it to store the possibilities
-                }else if(sum>0){
-                    k--;
-                }else{
-                    j++;
+        for(int i=0 ; i<n-2; i++){
+
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
+            int left= i+1;
+            int right = n-1;
+
+            while(left<right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
+
+                    while(left<right && nums[left] == nums[left+1]){
+                        left++;
+                    }
+                    while(left<right && nums[right] == nums[right-1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
                 }
-            } 
+                else if(sum>0){
+                    right--;
+                }
+                else{
+                    left++;
+                }
+            }
         }
-        L.addAll(s);
-        return L;
+        return ans;
     }
 }
