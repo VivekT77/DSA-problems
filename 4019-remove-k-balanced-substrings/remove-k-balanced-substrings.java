7c9e3dc26fd4f0bc9c915870b53df5ef9c592717
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-10-06 23:45:05 +0530

    Time: 35 ms (100.00%) | Memory: 45.8 MB (95.33%) - LeetSync

---
+++ 4019-remove-k-balanced-substrings/remove-k-balanced-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String removeSubstring(String s, int k) {
+
+        int n = s.length();
+        char[] stack = new char[n];
+        int top = -1;
+
+        for(char ch : s.toCharArray()){
+            stack[++top] = ch;
+
+            if(top >= 2*k -1){
+                boolean is_KBalanced = true;
+
+                for(int i=0;i<k;i++){
+                    if(stack[top-i] != ')' || stack[top-k-i] != '('){
+                        is_KBalanced = false;
+                        break;
+                    }
+                }
+
+                if(is_KBalanced){
+                    top = top - 2*k;
+                }
+            }
+        }
+        return new String(stack,0,top+1);
+    }
+}
