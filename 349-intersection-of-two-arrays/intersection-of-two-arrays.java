b253ad964f4408b93dedcb97d609d2c9f4b1e49a
VivekT77 <126587212+VivekT77@users.noreply.github.com>
2025-11-09 19:34:05 +0530

    Time: 2 ms (97.64%) | Memory: 44.9 MB (7.14%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,21 +1,23 @@
-//logic=> first make one hashset where store no. of nums1 in it and now check if the no.s of set is present in nums2 if there then add the no. in new arraylist and remove the particular no.s from the hashset now initialize new array where the the common elements from the arraylist will be displayed
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        ArrayList<Integer> result = new ArrayList<>();
-        for(int i=0;i<nums1.length;i++){
-            set.add(nums1[i]);
+        Set<Integer> s = new HashSet<>();
+        ArrayList<Integer> l = new ArrayList<>();
+
+        for(int num:nums1){
+            s.add(num);
         }
+
         for(int i=0;i<nums2.length;i++){
-            if(set.contains(nums2[i])){
-                result.add(nums2[i]);
-                set.remove(nums2[i]);
+            if(s.contains(nums2[i])){
+                l.add(nums2[i]);
+                s.remove(nums2[i]);
             }
         }
-         int[] ans = new int[result.size()];
-          for(int i=0;i<result.size();i++){
-            ans[i] =result.get(i);
-          } 
-          return ans; 
+
+        int[] result = new int[l.size()];
+        for(int i=0;i<l.size();i++){
+            result[i] = l.get(i);
+        }
+        return result;
     }
 }
